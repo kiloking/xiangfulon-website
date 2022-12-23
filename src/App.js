59fd39fbfa5f9ebe26cb43a8fa7fe3b1a8757e0d
wood.wu <kilokingw@gmail.com>
2022-12-23 18:00:11 +0800

    Updated path

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes> 
-        <Route path="/"  element={ <PublicPageLayout/>}>
+        <Route  exact path="/"  element={ <PublicPageLayout/>}>
           <Route path="" element={<Home />} />
           <Route path="about-1" element={<AboutOne />} />
           <Route path="about-2" element={<AboutTwo />} />
